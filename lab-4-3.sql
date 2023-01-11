b0c25b4470e3bfb4a207029df9e4daa1d4329502
Raeis22 <ruixianliu2024@u.northwestern.edu>
2023-01-11 23:47:15 +0000

    question

--- lab-4-3.sql
+++ lab-4-3.sql
@@ -4,7 +4,8 @@ FROM stats
     INNER JOIN players ON players.id = stats.player_id 
     INNER JOIN teams ON teams.id = stats.team_id
 WHERE teams.year = 2019
--- Expected result:
+--跟答案不一样，会答出唯一结论吗 
+--Expected result:
 --
 -- +---------------+------------+-----------+-----------+
 -- | New York Mets | Pete       | Alonso    | 53        |
